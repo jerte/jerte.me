c2726f9d5a75cc5cc71c61ff10b0d1ae1b078a89
joshrt <joshrt@MacBook-Air.local>
2020-06-24 09:19:50 -0400

    layout done

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var favicon = require('serve-favicon')
 
-express()
-	.use(express.static(path.join(__dirname, 'style_n_scripts')))
-	.use(express.static(path.join(__dirname, '/node_modules/jquery/dist/')))
-	.use(express.static(path.join(__dirname, 'views/partials')))
-	.set('views', path.join(__dirname, 'views'))
-	.set('view engine', 'ejs')
-	.get('/', (req,res) => res.render('pages/index'))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist/')))
+app.use(express.static(path.join(__dirname, '/node_modules/ejs/')))
+app.use(express.static(path.join(__dirname, 'views/partials')))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(favicon(path.join(__dirname, 'favicon/favicon.ico')))
+app.get('/', (req,res) => res.render('pages/index'))
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ static/script.js
@@ -0,0 +1,67 @@
+let nav = {"background": ["hometown", "education", "skills"],
+			"portfolio": ["projects", "github", "recent"],
+			"personal": ["contact", "music", "hobbies", "books"]};
+
+function renderContent(p) {
+
+	if( Object.keys(nav).includes(p) ) {
+	
+		let sc = nav[p];
+		$("#box").html(
+			ejs.render('<% sc.forEach(function(e) { %> <tr><td id=<%= e %>> <%= e %> </td></tr><% }); %>', {sc:sc}));
+		
+		$("#box td").css("background", $("#" + p).css("background"));
+		$("#box td").css("color", $("#content").css("background-color"));
+		$("#content").css("border-color", $("#" + p).css("background-color"));
+	
+	}
+}
+
+function renderSubContent(c, p) {
+	
+	if([].concat.apply([], Object.values(nav)).includes(p)) {
+		let sc = nav[c];
+		let first_table_row = "<tr><td id=" + sc[0] + ">" + sc[0] + "</td><td id=page " + 
+								"rowspan=" + sc.length + "</tr>";
+		sc = sc.slice(1);
+
+		$("#box").html( first_table_row +   
+			ejs.render(`<% sc.forEach(function(e) { %>
+						<tr><td id=<%= e %>> <%= e %> </td></tr>
+						<% }); %>`, {sc:sc}));
+
+		$("#box td").css("background", $("#" + c).css("background"));
+		$("#box td").css("color", $("#content").css("background-color"));
+		$("#content").css("border-color", $("#" + c).css("background-color"));
+		$("#" + p).html("<h2>" + p + "</h2>");
+		
+	}
+}
+
+$(document).ready(function() {
+	console.log("jquery loaded");
+	renderContent("portfolio");
+	let prevContent = null;
+	let currentContent = "portfolio";
+
+	$(".container").mouseover(function(event) {
+		renderContent(event.target.id);	
+		currentContent = event.target.id;
+	});
+
+	$("#box").mouseover(function(event) {
+
+		if([].concat.apply([], Object.values(nav)).includes(event.target.id)) {
+				
+			if(prevContent) {
+				$("#" + prevContent).html("<p>" + prevContent + "</p>");
+			}
+			prevContent = event.target.id;
+	
+			try {
+				renderSubContent(currentContent, event.target.id);
+			} catch (error) {
+			}
+		}		
+	});
+});
