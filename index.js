ec560489487548aa08199a4d28788fee5745b503
Josh Thomas <joshrthomas@protonmail.com>
2020-09-02 17:42:18 -0400

    working on adding admin system to manage interests page

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-var favicon = require('serve-favicon')
-const { Pool } = require('pg')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+var favicon = require('serve-favicon');
+const { Pool } = require('pg');
+
+var admin_active = false;
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
@@ -11,16 +14,61 @@ const pool = new Pool({
 	}
 })
 
-var app = express()
+var app = express();
 
-app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist/')))
-app.use(express.static(path.join(__dirname, '/node_modules/ejs/')))
+app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.use(express.static(path.join(__dirname, '/node_modules/ejs/')));
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(favicon(path.join(__dirname, 'favicon/favicon.ico')))
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(favicon(path.join(__dirname, 'favicon/favicon.ico')));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
+app.post('/admin/add-interest', async (req, res) => {
+	if(admin_active) {
+		try {
+			const client = await pool.connect();
+			client.query('INSERT INTO data VALUES (\'' + req.body.interest + '\', 5, 0)' );
+			client.release();
+			res.end("yes");
+		} catch (err) {
+			console.log(err);
+		}
+	}
+});
+
+app.post('/admin/add-interest-topic', async (req, res) => {
+	if(admin_active) {
+		try {
+			const client = await pool.connect();
+			// x topic id becomes id of coding_topics, need query beforehand
+			//client.query('INSERT INTO data VALUES (\'' + req.body.topic '\', x_topic_id, 0)' );
+			client.release();
+		} catch(err) {
+			console.log("Error: " + err);
+			res.send("Error: " + err);
+		}
+	}
+});
+
+app.get('/data/interest-topics/:topic', async (req, res) => {
+	try {
+		const client = await pool.connect();
+		const q = await client.query('select * from data where has_link=' + 
+			'(select id from data where name=\'' + req.params.topic + '\')');
+		console.log(q.rows);
+		res.send(q.rows);
+		
+	} catch (err) {
+		
+		res.send("Error: " + err);
+	}
+	
+});
 
 app.get('/home', (req, res) => { res.redirect('/')});
 
@@ -32,25 +80,33 @@ app.get('/:page?/:subpage?', async (req, res) => {
 		const client = await pool.connect();
 		const pages = await client.query('SELECT * FROM PAGES');
 		
-		console.log(pages.rows);
-
 		if(!req.params.page) {
 			req.params.page = "home";
 		}
 		
 		
 		var page_index = pages.rows.map(x => x['name'] ).indexOf(req.params.page);	
+		var subpage_index = pages.rows.map(x => x['name']).indexOf(req.params.page + '/' + req.params.subpage);
+
 		if(page_index >= 0) {
 		
 			for (var d_req of pages.rows[page_index]['data_req'].split(" ") ) {
 				const q = await client.query(
-						  'select * from site_data where site_id=' + 
-						  '(select id from site_data_names where name=\'' + d_req + '\')');
+						  'select * from data where name_id=' + 
+						  '(select id from data_names where name=\'' + d_req + '\')');
 				data[d_req] = q.rows;
 			}
-		
+			if(subpage_index >= 0) {
+				for( var d_req of pages.rows[subpage_index]['data_req'].split(" ") ) {
+					const q = await client.query(
+						'select * from data where name_id=' +
+						'(select id from data_names where name=\'' + d_req + '\')');
+					data[d_req] = q.rows;
+				}
+			}
+			
 			res.render('pages/base', 
-				{ page: req.params.page, subpage: req.params.subpage, data: data });
+				{ page: req.params.page, subpage: req.params.subpage, data: data, admin: admin_active });
 		} else {
 			res.render('pages/404');
 		}
@@ -59,6 +115,6 @@ app.get('/:page?/:subpage?', async (req, res) => {
 	} catch (err) {
 		res.send(err.message);
 	}
-})
+});
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- static/script.js
+++ static/script.js
@@ -51,5 +51,49 @@ $(document).ready(function() {
 	$("#git").click(function() {
 		window.location = $(this).find('a').attr('href');
 	});
+	
+	$("#add_interest").click(function() {
+		if($("#add_interest p").html()) {
+			
+			$("#add_interest").html(
+				"<form class=\"padding2 project\" id=\"add_interest_form\"><input id=\"new_interest\" type=\"text\">" + 
+				"<input type=\"submit\"></form>");
+			
+			$("#add_interest_form").submit(function(e) { 
+				$.post("/admin/add-interest", {interest: $("#new_interest").val() }, function(x) {
+					if(x=="yes") {
+						window.location = window.location;
+					}
+				});
+				
 
+				e.preventDefault();
+			});
+
+		}
+	});
+	
+	$(".interest-topic").click(function(e) {
+		//console.log(e.currentTarget.id);
+
+		if(e.currentTarget.id!='add-topic') {
+			$.get('/data/interest-topics/' + e.currentTarget.id, function(data) {
+				console.log(data[0]['name']);
+				$("#interest-topic-content").html("<p class=\"padding2\">" + data[0]['name'] + "</p>");		
+			}).fail(function(err) { console.log(err) });
+		} else if($("#add-topic span").html()) {
+			console.log('text');
+			$("#add-topic").html(
+				"<form id=\"add-topic-form\"><input type=\"text\" id=\"new_topic"><input type=\"submit\"></form>");
+			
+			$("#add-topic-form").submit(function(e) { 
+				$.post("/admin/add-interest-topic", { topic: $("#new_topic").val(), location: window.location}, 
+				function(x) {
+					if(x=='yes') {
+						window.location=window.location;
+					}
+				});
+			});
+		}
+	});
 });
